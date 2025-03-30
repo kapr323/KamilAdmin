451ed4a347959975d81af57c647ac07364d8c7c3
kamil <kamil.dvorak176@gmail.com>
2025-03-30 20:51:28 +0200

    nastavení kalendářů

--- viewer/forms.py
+++ viewer/forms.py
@@ -91,11 +91,6 @@ class EmployeeModelForm(ModelForm):
                   f"initial_surname = '{initial_surname}'")
         if not initial_name and not initial_surname:
             raise ValidationError("Je nutné zadat jméno a příjmení.")
-
-        initial_date_of_birth = cleaned_data.get('date_of_birth')
-        if initial_date_of_birth <= initial_date_of_birth:
-            raise ValidationError("Datum úmrtí nesmí být dřív, než datum narození.")
-
         return cleaned_data
 
 
